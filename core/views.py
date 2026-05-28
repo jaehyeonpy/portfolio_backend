70a44c0883d1e0eb3344131f7f0f366c9fd55581
jaehyeonpy <jaehyeonpy@gmail.com>
2026-05-29 02:04:04 +0900

    modify views.py to correspond to latency test; too many connection reqests to default mongodb, mongodb with connection pool, mongodb and redis with connection pool

--- core/views.py
+++ core/views.py
@@ -14,26 +14,31 @@ from core.util import *
 
 
 
-# Django does not provide a built-in view class returning an json, but only templates,
-# it is the only way to make a view returning an json that inherits the basic view class.
-
-
-
 myapp_logger = logging.getLogger("myapp")
-
 msg_prettifier = MessagePrettifier("django")
 
 
 
-class VideoPersonalityFetchView(View):
+
+
+
+# mongo client makes a connection pool having already open connections but specified as 0 for performance, 
+# redis client does not have already open ones.
+# mongo client opens a connection when the pool does not have any available one, redis client too.
+
+mongo_client = MongoClient(settings.MONGODB_HOST, maxPoolSize=10000) 		
+personality_db = mongo_client["personality"]
+character_a_personality_collection = personality_db["character_a"]
+
+redis_client = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True)
+
+
+
+# Django does not provide a built-in view class returning an json, but only templates,
+# it is the only way to make a view returning an json that inherits the basic view class.
+
+class VideoPersonalityFetchViewWithRedisConnectionpool(View):
 	def get(self, request, video_personality_doc_id):
-		mongo_client = MongoClient(settings.MONGODB_HOST) 		
-		personality_db = mongo_client["personality"]
-		character_a_personality_collection = personality_db["character_a"]
-
-		redis_client = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True)
-
-
 		video_personality_doc_cache = redis_client.hgetall(video_personality_doc_id)
 
 		if video_personality_doc_cache != {}:
@@ -41,15 +46,14 @@ class VideoPersonalityFetchView(View):
 			myapp_logger.info(f'a cache hit:\n{prettifed_cache}') 
 			return JsonResponse(video_personality_doc_cache, json_dumps_params={"ensure_ascii": False})
 
-
 		video_personality_doc = character_a_personality_collection.find_one({"_id": video_personality_doc_id})
 		prettifed_doc = msg_prettifier.prettify_json(video_personality_doc)
 		myapp_logger.info(f'fetched from mongodb:\n{prettifed_doc}') 
 
 
 		# writing the fetched document/record to redis,
-		# implementing an optimistic lock in case of multiple writings.
-
+		# implementing an optimistic lock in case of concurrent writings.
+		
 		with redis_client.pipeline() as p:
 			try:
 				p.watch(video_personality_doc["_id"])
@@ -71,3 +75,28 @@ class VideoPersonalityFetchView(View):
 		return JsonResponse(video_personality_doc, json_dumps_params={"ensure_ascii": False})
 	
 
+
+class VideoPersonalityFetchViewWithDefaultMongoDB(View):
+	def get(self, request, video_personality_doc_id):
+		# as this client is different from the global client, this client does not belong to the connection pool.
+		# this client is created for each user request, makes each connection per each request.
+		mongo_client = MongoClient(settings.MONGODB_HOST, connectTimeoutMS=None) 		
+		personality_db = mongo_client["personality"]
+		character_a_personality_collection = personality_db["character_a"]
+
+		video_personality_doc = character_a_personality_collection.find_one({"_id": video_personality_doc_id})
+		prettifed_doc = msg_prettifier.prettify_json(video_personality_doc)
+		myapp_logger.info(f'fetched from mongodb:\n{prettifed_doc}') 
+
+		return JsonResponse(video_personality_doc, json_dumps_params={"ensure_ascii": False})
+
+
+
+class VideoPersonalityFetchViewWithMongoDBConnectionpool(View):
+	def get(self, request, video_personality_doc_id):
+		# this view uses the global mongodb client, which has a connection pool.
+		video_personality_doc = character_a_personality_collection.find_one({"_id": video_personality_doc_id})
+		prettifed_doc = msg_prettifier.prettify_json(video_personality_doc)
+		myapp_logger.info(f'fetched from mongodb:\n{prettifed_doc}') 
+
+		return JsonResponse(video_personality_doc, json_dumps_params={"ensure_ascii": False})
