a505cf974a7b2cd0d374ad7ec53ea57faf9af553
jaehyeonpy <jaehyeonpy@gmail.com>
2026-06-22 03:53:54 +0900

    delete unnecessary additional files from the merge commit right before; because of moving the location of the original django project

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from core.views import *
-
-
-urlpatterns = [
-	path('<str:video_personality_doc_id>/', VideoPersonalityFetchViewWithRedisConnectionpool.as_view()),
-]
